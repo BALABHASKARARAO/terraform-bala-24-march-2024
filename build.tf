32aa794934c1e3eaf31885a034db58803648e187
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2023-09-05 06:36:52 +0000

    Update build.tf

--- build.tf
+++ build.tf
@@ -9,6 +9,9 @@ resource "docker_image" "terraform" {
 }
 
 resource "docker_container" "kishore" {
+  lifecycle {
+   create_before_destroy = true
+  }
   name  = "checking"
   image = docker_image.terraform.image_id
 
