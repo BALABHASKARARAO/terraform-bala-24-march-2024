3b8747b84ec7de8cbf8033bddb714f5d7768c332
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2024-03-26 04:19:08 +0530

    Update registry.tf

--- registry.tf
+++ registry.tf
@@ -1,8 +1,1 @@
 
-resource "null_resource" "docker_login" {
-  provisioner "local-exec" {
-  command = <<EOT
-    echo "${var.password}" | docker login -u "${var.username}" --password-stdin
-  EOT
-  }
-}
