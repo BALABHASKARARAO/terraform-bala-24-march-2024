ca8a70140ae188fea2cfcdbb0f8d8fa19e782221
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2024-03-26 04:29:20 +0530

    Update push.tf

--- push.tf
+++ push.tf
@@ -1,1 +1,9 @@
+resource "null_resource" "push_to_registry" {
+  triggers = {
+    image_id = docker_image.terraform.image_id
+  }
 
+  provisioner "local-exec" {
+    command = "docker push balabhaskararao/2-sep:latest" # Replace with your registry URL and image name
+  }
+}
